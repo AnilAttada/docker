5f6990d917de781133a5d2e41c0aa39683097c64
AnilAttada <anilattada222@gmail.com>
2026-05-21 07:02:49 +0530

    DOCKER

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "aws_instance" "docker" {
 
 resource "aws_security_group" "allow_all_docker" {
   name        = "allow_all_docker"
-  description = "Allow all traffic"
+  description = "Allow all traffic to docker"
 
     ingress {
     from_port        = 0
